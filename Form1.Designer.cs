9b030bc71d245c2241aa14fc49f9306d3412069e
7jjin <wlsgud6305@naver.com>
2024-08-08 00:33:06 +0900

    Feat : 서버완성

---
+++ Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace FTP_Server_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.createIdBox = new System.Windows.Forms.TextBox();
+            this.createPwBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.connectIPBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.connectIDBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.connectPWBox = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel3, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(633, 434);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.createPwBox);
+            this.panel1.Controls.Add(this.createIdBox);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(627, 80);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.connectPWBox);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.connectIDBox);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.connectIPBox);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 89);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(627, 124);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.listBox1);
+            this.panel3.Controls.Add(this.richTextBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(3, 219);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(627, 212);
+            this.panel3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "생성";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "PW";
+            // 
+            // createIdBox
+            // 
+            this.createIdBox.Location = new System.Drawing.Point(114, 22);
+            this.createIdBox.Name = "createIdBox";
+            this.createIdBox.Size = new System.Drawing.Size(217, 25);
+            this.createIdBox.TabIndex = 3;
+            // 
+            // createPwBox
+            // 
+            this.createPwBox.Location = new System.Drawing.Point(114, 52);
+            this.createPwBox.Name = "createPwBox";
+            this.createPwBox.PasswordChar = '*';
+            this.createPwBox.Size = new System.Drawing.Size(217, 25);
+            this.createPwBox.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(490, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "생성";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(490, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "삭제";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "연결";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "IP";
+            // 
+            // connectIPBox
+            // 
+            this.connectIPBox.Location = new System.Drawing.Point(63, 50);
+            this.connectIPBox.Name = "connectIPBox";
+            this.connectIPBox.Size = new System.Drawing.Size(181, 25);
+            this.connectIPBox.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(37, 86);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 15);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "ID";
+            // 
+            // connectIDBox
+            // 
+            this.connectIDBox.Location = new System.Drawing.Point(63, 83);
+            this.connectIDBox.Name = "connectIDBox";
+            this.connectIDBox.Size = new System.Drawing.Size(181, 25);
+            this.connectIDBox.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(271, 53);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 15);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "PW";
+            // 
+            // connectPWBox
+            // 
+            this.connectPWBox.Location = new System.Drawing.Point(308, 50);
+            this.connectPWBox.Name = "connectPWBox";
+            this.connectPWBox.PasswordChar = '*';
+            this.connectPWBox.Size = new System.Drawing.Size(145, 25);
+            this.connectPWBox.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(490, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "서버접속";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(22, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(398, 190);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(426, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(192, 184);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 434);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox createPwBox;
+        private System.Windows.Forms.TextBox createIdBox;
+        private System.Windows.Forms.TextBox connectIPBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox connectPWBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox connectIDBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,229 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FTP_Server_Project
+{
+    public partial class Form1 : Form
+    {
+        TcpListener server = null; // 서버
+        TcpClient clientSocket = null; // 소켓
+
+        string date; // 날짜
+        private Dictionary<TcpClient, string> clientList = new Dictionary<TcpClient, string>(); // 각 클라이언트 마다 리스트에 추가
+        private int PORT = 21; // 포트 정보
+     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitForm();
+            this.Text = "Server";
+            connectIPBox.Text = "";
+            connectIPBox.Text = GetLocalIP();
+        }
+        
+        // 폼 초기화
+        private void InitForm()
+        {
+            connectIPBox.Text = "";
+            connectIDBox.Text = "";
+            connectPWBox.Text = "";
+            richTextBox1.Text = "";
+            listBox1.Items.Clear();
+        }
+
+        // 로컬IP 가져오기
+        private string GetLocalIP()
+        {
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            string localIP = string.Empty;
+
+            for (int i = 0; i < host.AddressList.Length; i++)
+            {
+                if (host.AddressList[i].AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = host.AddressList[i].ToString();
+                    break;
+                }
+            }
+
+            return localIP;
+        }
+
+        // 서버 실행 버튼
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Thread thread1 = new Thread(InitSocket);
+            thread1.IsBackground = true;
+            thread1.Start();
+        }
+
+        // 소켓 초기화
+        private void InitSocket()
+        {
+            server = new TcpListener(IPAddress.Parse(connectIPBox.Text), PORT); // 서버 객체 생성 및 IP주소와 Port번호를 할당
+            clientSocket = default(TcpClient); // 소켓 설정
+            server.Start(); // 서버 시작
+            DisplayText("System : Server 시작");
+
+            while (true)
+            {
+                try
+                {
+                    clientSocket = server.AcceptTcpClient(); // client 소켓 접속 허용
+
+                    NetworkStream stream = clientSocket.GetStream();
+                    byte[] buffer = new byte[1024]; // 버퍼
+                    int bytes = stream.Read(buffer, 0, buffer.Length);
+                    string userName = Encoding.Unicode.GetString(buffer, 0, bytes);
+                    userName = userName.Substring(0, userName.IndexOf("$")); // client 사용자 명
+                    DisplayText("System : [" + userName + "] 접속");
+
+                    clientList.Add(clientSocket, userName); // cleint 리스트에 추가
+                    SendMessageAll(userName + " 님이 입장하셨습니다.", "", false); // 모든 client에게 메세지 전송
+                    SetUserList(userName, "I");
+
+                    HandleClient h_client = new HandleClient(); // 클라이언트 추가
+                    h_client.OnReceived += new HandleClient.MessageDisplayHandler(OnReceived);
+                    h_client.OnDisconnected += new HandleClient.DisconnectedHandler(h_client_OnDisconnected);
+                    h_client.startClient(clientSocket, clientList);
+                }
+                catch (SocketException se) { break; }
+
+                catch (Exception ex) { break; }
+            }
+
+            clientSocket.Close(); // client 소켓 닫기
+
+            server.Stop(); // 서버 종료
+        }
+
+        // client 접속 해제 
+        private void h_client_OnDisconnected(TcpClient clientSocket)
+        {
+            if (clientList.ContainsKey(clientSocket))
+            {
+                clientList.Remove(clientSocket);
+            }
+        }
+
+        // client로 부터 받은 메세지
+        private void OnReceived(string message, string userName)
+        {
+            if (message.Equals("LeaveChat"))
+            {
+                string displayMessage = "Leave user : " + userName;
+
+                DisplayText(displayMessage);
+                SendMessageAll("LeaveChat", userName, true);
+                SetUserList(userName, "D");
+            }
+            else
+            {
+                string displayMessage = "From Client : " + userName + " : " + message;
+                DisplayText(displayMessage); // Server단에 출력
+                SendMessageAll(message, userName, true); // 모든 Client에게 전송
+            }
+        }
+
+        // 메세지 전송
+        public void SendMessageAll(string message, string userName, bool flag)
+        {
+            foreach (var pair in clientList)
+            {
+                date = DateTime.Now.ToString("yyyy.MM.dd. HH:mm:ss"); // 현재 날짜 받기
+
+                TcpClient client = pair.Key as TcpClient;
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = null;
+
+                if (flag)
+                {
+                    if (message.Equals("LeaveChat"))
+                        buffer = Encoding.Unicode.GetBytes(userName + " 님이 대화방을 나갔습니다.");
+                    else
+                        buffer = Encoding.Unicode.GetBytes("[ " + date + " ] " + userName + " : " + message);
+                }
+                else
+                {
+                    buffer = Encoding.Unicode.GetBytes(message);
+                }
+                stream.Write(buffer, 0, buffer.Length); // 버퍼 쓰기
+                stream.Flush();
+            }
+        }
+
+        // 화면에 text 출력
+        private void DisplayText(string text)
+        {
+            richTextBox1.Invoke((MethodInvoker)delegate { richTextBox1.AppendText(text + "\r\n"); }); // 데이터를 수신창에 표시, 데이터 충돌을 피하기 위해 Invoke 사용
+            richTextBox1.Invoke((MethodInvoker)delegate { richTextBox1.ScrollToCaret(); });  // 스크롤을 제일 아래로 설정
+        }
+
+        // 접속 client 출력
+        private void SetUserList(string userName, string div)
+        {
+            try
+            {
+                if (div.Equals("I"))
+                    listBox1.Items.Add(userName);
+                else if (div.Equals("D"))
+                    listBox1.Items.Remove(userName);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        // FTP 키 생성
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(createIdBox.Text))
+            {
+                MessageBox.Show("ID를 입력해주세요.");
+                createIdBox.Focus();
+                return;
+            }else if (string.IsNullOrEmpty(createPwBox.Text))
+            {
+                MessageBox.Show("PW를 입력해주세요.");
+                createPwBox.Focus();
+                return;
+            }
+            else if (Registry.CurrentUser.OpenSubKey(@"SoftWare\FTP") != null)
+            {
+                MessageBox.Show("FTP 키가 이미 존재합니다.");
+                return;
+            }
+            RegistryKey ftpValue;
+            ftpValue = Registry.CurrentUser.CreateSubKey("SoftWare").CreateSubKey("FTP");
+            ftpValue.SetValue("FTP_ID", this.createIdBox.Text);
+            ftpValue.SetValue("FTP_PW", this.createPwBox.Text);
+            MessageBox.Show("FTP 키가 발급되었습니다.");
+        }
+
+        // FTP 키 삭제
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Registry.CurrentUser.CreateSubKey("Software").DeleteSubKey("FTP");
+            MessageBox.Show("FTP 키가 삭제되었습니다.");
+            createIdBox.Clear();
+            createPwBox.Clear();
+        }
+    }
+}
